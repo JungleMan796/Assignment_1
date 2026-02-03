d2ff6ac72433116953f6b1fa67d7ca5e3acc3b2b
JungleMan796 <callumpeterwilliamson08@gmail.com>
2026-02-03 13:52:31 +0000

    Setup of main function and #includes.

--- ass1_test.cpp
+++ ass1_test.cpp
@@ -1,4 +1,10 @@
-// Test document for Assignment 1.
-// Callum Williamson - 11399397
-//03/02/2026
+// Test document for Assignment 1, calculating transition energy using Bohr formula.
+// Callum Williamson - 11399397.
+//03/02/2026.
 
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+}
